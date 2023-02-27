83b0673a44d9c166029417336d80f04d5ea1a5e8
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-27 22:03:03 +0000

    Initial Commit

---
+++ Activity Suggestor/Activity_SuggestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_SuggestorApp.swift
+//  Activity Suggestor
+//
+//  Created by Don Bouncy on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Activity_SuggestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
